0e9de6de5955cb019e3bb4538b224243f058a9a7
youss-uiux <youssif.aboubacar@outlook.fr>
2025-02-15 11:08:24 +0100

    Add conditional logic to evaluate variable 'a' and print corresponding messages

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,18 @@
-print("Hello world")
+a=190
+
+if(a>=100):
+    print("a est supérieur ou égal à 100")
+    a=a-100
+    print("a vaut maintenant",a)
+elif(a>100):
+    print("a est supérieur à 100")
+    if(a>150):
+        print("a est supérieur à 100")
+elif(a!=100):
+    print("a est différent 100")
+elif(a<100):
+    print("a est inférieur à 100")
+elif(a<=100):
+    print("a est inférieur ou égal à 100")
+else:
+    print("a est égal à 100")
